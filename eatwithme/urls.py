0d727d17a84171bec942dd1dc0188ae5b1913205
unknown <cjy0565@kookmin.ac.kr>
2020-05-23 16:26:49 +0900

    home

--- eatwithme/urls.py
+++ eatwithme/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 import main.urls
-import account.urls
+# import account.urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('main/', include('main.urls')),
-    path('account/', include('account.urls')),
+    # path('account/', include('account.urls')),
 ]
