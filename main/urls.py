94924980b9a80023d536a3caf630bd938fd5d259
unknown <cjy0565@kookmin.ac.kr>
2020-05-26 17:09:59 +0900

    거의 다했어요

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('home/',views.home, name = "home"),
+    path('new/', views.new, name="new"),
+    path('board/', views.board, name="board"),
+    path('detail/<int:board_id>', views.detail, name="detail"),
+    path('create/', views.create, name='create'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from .models import Board
+from django.utils import timezone
 
 # Create your views here.
 
 def home(request) :
     return render(request, 'home.html')
+
+def new(request) :
+    return render(request, 'new.html')
+
+def create(request) :
+    new_board = Board()
+    new_board.title = request.POST.get('title')
+    new_board.pub_date = timezone.datetime.now()
+    new_board.body= request.POST['body']
+    # new_board.meeting_time = request.POST['meeting_time']
+    new_board.number = request.POST['number']
+    new_board.location = request.POST['location']
+    new_board.save()
+    return redirect('board')
+
+def board(request) :
+
+    boards= Board.objects.all()
+
+    return render(request, 'board.html',{'boards':boards})
+
+def detail(request,board_id) :
+    board = get_object_or_404(Board, pk = board_id) 
+    return render(request, 'detail.html',{'board':board})
